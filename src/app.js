c9b3790bdd1f5811f247e7c9b8705482fc630444
Jen Nummerdor <jennifer.nummerdor@gmail.com>
2019-04-02 13:35:55 -0700

    Not many changes made

--- src/app.js
+++ src/app.js
@@ -8,11 +8,5 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.urlencoded({extended: true})) 
 app.use(bodyParser.json())
 
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
 
 module.exports = app;
