99a70478a95bc1da813eb8081c3cfc69c112eec1
Jen Nummerdor <jennifer.nummerdor@gmail.com>
2019-04-02 16:00:11 -0700

    proxy server is WORKINGgit add . so cool! :tada:

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const proxy = require('http-proxy-middleware');
-const { routes } = require('./config.json');
 const cors = require('cors')
 
 const app = express()
@@ -11,16 +9,5 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.urlencoded({extended: true})) 
 app.use(bodyParser.json())
-
-for (route of routes) {
-  app.use(route.route,
-    proxy({
-      target: route.address,
-      pathRewrite: (path, req) => {
-        return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
-      }
-    })
-  );
-}
 
 module.exports = app;
